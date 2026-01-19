7c29dd234dda05a56dcc7ba34cbcbbbc4b7aeb4e
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-19 15:20:44 +0100

    test

--- find-files.sh
+++ find-files.sh
@@ -1,1 +1,1 @@
-curl -s https://assets.01-edu.org/devops-branch/find-files-example.zip --output out && unzip -q out && cd find-files-example && find . -type f \( -name '*z' \) && find . -name 'a*' ! -name '*z' && rm -rf ../find-files-example ../out
+curl -s https://assets.01-edu.org/devops-branch/find-files-example.zip --output out && unzip -q out && cd find-files-example && find . -type f \( -name '*z' \) && find . -name 'a*' && rm -rf ../find-files-example ../out
