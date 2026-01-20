e8dd85ed28f07d0fa5ca9f23fdf891982745e461
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-20 15:14:16 +0100

    check

---
+++ set-internal-vars.sh
@@ -0,0 +1,16 @@
+MY_MESSAGE="Hello World"
+MY_NUM=100
+MY_PI=3.142
+MY_ARR=(one two three four five)
+echo $MY_MESSAGE
+echo $MY_NUM
+echo $MY_PI
+# echo $MY_ARR
+for item in "${MY_ARR[@]}"; do
+    echo -n "$item"
+    if [[ "$item" == "five" ]]; then
+        break
+    fi
+    echo -n ", "
+done
+echo ""
