89d173222eab7f4b91884fa15be498f680779bdb
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-20 16:03:29 +0100

    echo "${MY_ARR[*]}"

--- set-internal-vars.sh
+++ set-internal-vars.sh
@@ -1,22 +1,13 @@
+#!/bin/bash
+
 MY_MESSAGE="Hello World"
 MY_NUM=100
 MY_PI=3.142
-MY_ARR=(one two three four five)
+MY_ARR=(one, two, three, four, five)
 
 
 echo $MY_MESSAGE
 echo $MY_NUM
 echo $MY_PI
-# echo $MY_ARR
-for item in "${MY_ARR[@]}"; do
-    echo -n "$item"
-    if [[ "$item" == "five" ]]; then
-        break
-    fi
-    echo -n ", "
-done
-echo ""
-
-
-
+echo "${MY_ARR[*]}"
 
