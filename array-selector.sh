ea1d16806b9bfd811817276630cfe0b7e01437b8
babdelil <bouchikhiabdellah0@gmail.com>
2026-01-22 15:16:09 +0100

    fix: change exit status from 1 to 0 on error in array-selector script

--- array-selector.sh
+++ array-selector.sh
@@ -2,6 +2,6 @@
 name=(red blue green white black)
 if [[  $1 -lt 1 || $1 -ge $((${#name[@]} + 1)) || ! $1 =~ ^[0-9]+$ || "$#" -ne 1 ]]; then
     echo "Error"
-    exit 1
+    exit 0
 fi
 echo "${name[$1-1]}"
