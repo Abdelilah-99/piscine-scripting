eae47bf70ab6685f70abb930e5547a80fcfdc977
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-23 15:46:13 +0100

    fix: update exit status for argument count and validation checks in comparator script

--- comparator.sh
+++ comparator.sh
@@ -1,10 +1,10 @@
 
 if [[ "$#" != 1 ]]; then
     echo "Error: The script only works with two arguments!"
-    exit 1
+    exit 0
 elif [[ ! "$1" =~ ^[+-]?[0-9]+$ || ! "$2" =~ ^[+-]?[0-9]+$ ]]; then
     echo "Error: Only two numeric arguments are acceptable!"
-    exit 1
+    exit 0
 fi
 if [[ "$1" -gt "$2" ]]; then
     echo "$1 > $2"
