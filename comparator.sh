d352d3e96680a391c3bebdff94b4bcf2ed3f3c45
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-23 15:43:00 +0100

    fix: simplify conditional statements in comparator script

--- comparator.sh
+++ comparator.sh
@@ -9,10 +9,8 @@ if [[ ! "$1" =~ ^[+-]?[0-9]+$ || ! "$2" =~ ^[+-]?[0-9]+$ ]]; then
 fi
 if [[ "$1" -gt "$2" ]]; then
     echo "$1 > $2"
-fi
-if [[ "$1" -lt "$2" ]]; then
+elif [[ "$1" -lt "$2" ]]; then
     echo "$1 < $2"
-fi
-if [[ "$1" -eq "$2" ]]; then
+elif [[ "$1" -eq "$2" ]]; then
     echo "$1 = $2"
 fi
