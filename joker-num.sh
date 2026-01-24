a0bb155fad1346e3b586a60b131d5eee989d1e51
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-24 13:56:38 +0100

    fix: add input validation for user guesses in number guessing game

--- joker-num.sh
+++ joker-num.sh
@@ -7,17 +7,19 @@ s=$1
     fi
 while true; do
     read -p "Enter your guess ($c tries left): " n
-    c=$((c - 1))
-    if [[ $n -gt $s ]]; then
-        echo "Go down"
-    elif [[ $n -lt $s ]]; then
-        echo "Go up"
-    elif [[ $n == $s ]]; then
-        echo "Congratulations, you found the number in $c moves!"
-        exit 0
-    fi
-    if [[ $c == 0 ]]; then
-        echo "You lost, the number was $s"
-        exit 0
+    if [[ "$n" =~ [0-9]+$ ]]; then
+        c=$((c - 1))
+        if [[ $n -gt $s ]]; then
+            echo "Go down"
+        elif [[ $n -lt $s ]]; then
+            echo "Go up"
+        elif [[ $n == $s ]]; then
+            echo "Congratulations, you found the number in $c moves!"
+            exit 0
+        fi
+        if [[ $c == 0 ]]; then
+            echo "You lost, the number was $s"
+            exit 0
+        fi
     fi
 done
