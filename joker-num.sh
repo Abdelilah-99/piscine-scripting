1901d98d44e20c682fbf86de1f82c28fd001c1ad
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-24 13:59:23 +0100

    fix: enhance input validation and guessing logic in number guessing game

--- joker-num.sh
+++ joker-num.sh
@@ -1,32 +1,34 @@
-c=10
-s=$1
+secret=$1
+tries=5
 
-if [[ "$#" -ne 1 || $s -gt 100 || $s -lt 1 ]]; then
+if [[ "$#" -ne 1 || ! "$secret" =~ ^[0-9]+$ || $secret -lt 1 || $secret -gt 100 ]]; then
     echo "Error: wrong argument"
     exit 1
 fi
 
-while true; do
-    read -p "Enter your guess ($c tries left): " n
+for ((i=1; i<=tries; )); do
+    remaining=$((tries - i + 1))
+    echo -n "Enter your guess ($remaining tries left): "
+    read guess
 
-    if [[ "$n" =~ ^[0-9]+$ ]]; then
-        c=$((c - 1))
-
-        if [[ $n -gt $s ]]; then
-            echo "Go down"
-        elif [[ $n -lt $s ]]; then
-            echo "Go up"
-        else
-            moves=$((10 - c))
-            echo "Congratulations, you found the number in $moves moves!"
-            exit 0
-        fi
-
-        if [[ $c -eq 0 ]]; then
-            echo "You lost, the number was $s"
-            exit 0
-        fi
-    else
-        echo "Invalid input, enter a number"
+    if [[ -z "$guess" || ! "$guess" =~ ^[0-9]+$ ]]; then
+        continue
     fi
+
+    if [[ $guess -lt 1 || $guess -gt 100 ]]; then
+        continue
+    fi
+
+    if [[ $guess -gt $secret ]]; then
+        echo "Go down"
+    elif [[ $guess -lt $secret ]]; then
+        echo "Go up"
+    else
+        echo "Congratulations, you found the number in $i moves!"
+        exit 0
+    fi
+
+    ((i++))
 done
+
+echo "You lost, the number was $secret"
