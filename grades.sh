c07bc216d77070bca6189a2ee66af8ef09186428
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-24 15:05:00 +0100

    fix: enhance grade validation to include grades above 100 and streamline input handling

--- grades.sh
+++ grades.sh
@@ -1,25 +1,36 @@
+#!/bin/bash
+
 studentnum=$1
-if [[ "$#" -ne 1 ]]; then
+studentname=()
+studentgrade=()
+
+# Argument validation
+if [[ "$#" -ne 1 || ! "$studentnum" =~ ^[0-9]+$ || $studentnum -le 0 ]]; then
     echo "Error: expect 1 argument only!"
     exit 1
 fi
-for ((i=1; i <= studentnum; )); do
-    # echo -n 
+
+for ((i=1; i<=studentnum; i++)); do
     read -p "Student Name #$i: " name
-    # echo -n "Student Grade #$i: "
     read -p "Student Grade #$i: " grade
-    if [[ ! "$grade" =~ ^[0-9]+$ || $grade -lt 0 ]]; then
+
+    if [[ ! "$grade" =~ ^[0-9]+$ || $grade -lt 0 || $grade -gt 100 ]]; then
         echo "Error: The grade $grade is not a valid input. Only numerical grades between 0 and 100 are accepted."
         exit 1
     fi
-    if [[ $grade -ge 90 ]]; then
-        echo "$name: You did an excellent job!"
-    elif [[ $grade -ge 70 ]]; then
-        echo "$name: You did a good job!"    
-    elif [[ $grade -ge 50 ]]; then
-        echo "$name: You need a bit more effort!"
-    elif [[ $grade -lt 50 ]]; then
-        echo "$name: You had a poor performance!"       
+
+    studentname+=("$name")
+    studentgrade+=("$grade")
+done
+
+for ((i=0; i<studentnum; i++)); do
+    if [[ ${studentgrade[i]} -ge 90 ]]; then
+        echo "${studentname[i]}: You did an excellent job!"
+    elif [[ ${studentgrade[i]} -ge 70 ]]; then
+        echo "${studentname[i]}: You did a good job!"
+    elif [[ ${studentgrade[i]} -ge 50 ]]; then
+        echo "${studentname[i]}: You need a bit more effort!"
+    else
+        echo "${studentname[i]}: You had a poor performance!"
     fi
-    ((i++))
 done
