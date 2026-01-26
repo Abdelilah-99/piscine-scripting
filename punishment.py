7ac56208e4f17d6064a1399d43de9d890727fbbc
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-26 12:07:11 +0100

    feat: modify do_punishment function to return formatted punishment messages instead of printing

--- punishment.py
+++ punishment.py
@@ -1,3 +1,5 @@
 def do_punishment(first_part, second_part, nb_lines):
+    rs = ""
     for i in range(nb_lines):
-        print(str.strip(first_part) + " " + str.strip(second_part) + ".")
+        rs += str.strip(first_part) + " " + str.strip(second_part) + ".\n"
+    return rs
