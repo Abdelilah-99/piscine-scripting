55c911b79084fe254f3ebf03853438a9735c52fe
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-26 12:11:42 +0100

    feat: add zero-check for divide and integer_division functions

--- numerical_operations_the_return.py
+++ numerical_operations_the_return.py
@@ -1,6 +1,11 @@
 def modulo(a, b):
     return a%b
 def divide(a, b):
+    if (b == 0):
+        return 0
     return a/b
 def integer_division(a, b):
+    if (b == 0):
+        return 0
     return a//b
+# print(divide(10, 0))
