145c91488c8b8a304d464a136ec15e51c8a41134
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-26 12:33:12 +0100

    fix: correct capitalize function call and ensure 'Milk' is consistently formatted in clean_list

--- clean_the_list.py
+++ clean_the_list.py
@@ -5,11 +5,11 @@ def clean_list(shopping_list):
     if (shopping_list == []):
         return []
     for item in shopping_list:
-        list.append(str(i) + "/ " + str.strip(item).capitalize)
+        list.append(str(i) + "/ " + str.strip(item).capitalize())
         i+=1
         if (item == 'milk'):
             f = 1
     if f == 0:
-        list.append(str(i) + "/ " + 'milk')
+        list.append(str(i) + "/ " + 'Milk')
     return list
-# print(clean_list(['test', 'test123']))
+print(clean_list(['test', 'test123']))
