7a9f1e1930a191b3ea5213c6a8926f157711734c
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-27 16:20:00 +0100

    fix: correct variable initialization and streamline merge logic in merge_two function

--- merge_two.py
+++ merge_two.py
@@ -1,19 +1,17 @@
 import json
 
 def merge_two(first_dict):
-    new_dic = first_dict
-    while (1 == 1):
-        print("Add a new entry:")
-        # print("key: ", end="")
-        key = input("key: ")
-        if (key == "exit"):
-            break
-        # print("value: ", end="")
-        value = int(input("value: "))
-        new_dic = {**first_dict, **{key: value}}
-    # print(json.dumps(new_dic))
-    if new_dic == {}:
-        return {}
-    return new_dic
+    new_dict = {}
 
-# merge_two({"Ahmed": 15})
+    while True:
+        print("Add a new entry:")
+        key = input("key: ")
+
+        if key == "exit":
+            break
+
+        value = int(input("value: "))
+        new_dict[key] = value
+
+    merged = {**first_dict, **new_dict}
+    return json.dumps(merged)
