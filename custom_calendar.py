e09580d21cebd00476c593f2d00d077f6cd866a2
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-27 11:27:06 +0100

    fix: return None instead of "None" in day_from_number and day_to_number functions

--- custom_calendar.py
+++ custom_calendar.py
@@ -10,15 +10,15 @@ days = {
 
 def day_from_number(day_number):
     if (day_number < 1 or day_number > 7):
-        return "None"
+        return None
     return days[day_number]
 
 def day_to_number(day):
     if day not in days.values():
-        return "None"
+        return None
     for number, name in days.items():
         if name.lower() == day.lower():
             return number
 
-# print(day_from_number(5))
-# print(day_to_number("Saturday"))
+# print(day_from_number(8))
+# print(day_to_number("Satsurday"))
