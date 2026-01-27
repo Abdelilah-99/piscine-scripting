f4e673000cbbb9cf59f44ed93180c220ca1fadd2
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-27 15:21:11 +0100

    fix: improve input prompts for clarity in merge_two function

--- merge_two.py
+++ merge_two.py
@@ -4,13 +4,13 @@ def merge_two(first_dict):
     new_dic = first_dict
     while (1 == 1):
         print("Add a new entry:")
-        print("key: ", end="")
-        key = input()
+        # print("key: ", end="")
+        key = input("key: ")
         if (key == "exit"):
             break
-        print("value: ", end="")
-        value = int(input())
+        # print("value: ", end="")
+        value = int(input("value: "))
         new_dic = {**first_dict, **{key: value}}
     print(json.dumps(new_dic))
 
-# merge_two({"Ahmed": 15})
+merge_two({"Ahmed": 15})
