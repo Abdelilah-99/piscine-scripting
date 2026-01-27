d21b5dd16c1f0c9e804eb21630116dbbee7815fb
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-27 11:22:39 +0100

    feat: add custom calendar functions to convert between day numbers and names

---
+++ custom_calendar.py
@@ -0,0 +1,24 @@
+days = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday"
+}
+
+def day_from_number(day_number):
+    if (day_number < 1 or day_number > 7):
+        return "None"
+    return days[day_number]
+
+def day_to_number(day):
+    if day not in days.values():
+        return "None"
+    for number, name in days.items():
+        if name.lower() == day.lower():
+            return number
+
+# print(day_from_number(5))
+# print(day_to_number("Saturday"))
