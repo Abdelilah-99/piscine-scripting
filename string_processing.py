5311fa5982c4e75c11619cd8f48e7344521591a6
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-28 15:50:04 +0100

    fix: improve tokenize function to remove empty strings from output

--- string_processing.py
+++ string_processing.py
@@ -4,6 +4,9 @@ def tokenize(sentence):
     pattern = r"[^A-Za-z0-9]+"
     replaced = re.sub(pattern, " ", sentence)
     arr = replaced.split(" ")
-    return arr
-
+    rs = []
+    for v in arr:
+        if v != '':
+            rs.append(v)
+    return rs
 # tokenize("abdelilah,bouchikhi test!!")
